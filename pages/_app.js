8a56299bcb7331ed6973d053471b842f2deb1f34
Mohammed Monirul Islam <monirul.islam@doe.virginia.gov>
2026-05-01 11:38:43 -0400

    Updated vulnerability fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,14 @@
-import { GoogleAdSense } from "nextjs-google-adsense";
-
+import Head from "next/head"
+import '@/styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.css'
 
-import "../styles/globals.css";
-
-import { useEffect } from "react";
-
-export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    require("bootstrap/dist/js/bootstrap.bundle.min.js");
-  }, []);
-
-  return <><GoogleAdSense publisherId="ca-pub-1521514346848136" /><Component {...pageProps} /></>;
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
